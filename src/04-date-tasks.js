49c34f5715f5e1451fc8abab4235cf8296306a11
leonarci <artem.janicki@gmail.com>
2022-07-26 00:49:40 +0300

    feat: date task

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -83,8 +83,10 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  const ms = (Math.abs(startDate.getTime() - endDate.getTime()));
+  const strTime = (new Date(ms)).toISOString();
+  return strTime.slice(strTime.indexOf('T') + 1, strTime.indexOf('Z'));
 }
 
 
